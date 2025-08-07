eda06aa666d08b4195df82058f400bd66e3b34a7
kawofong <14829553+kawofong@users.noreply.github.com>
2025-08-07 09:30:01 -0400

    feat(java): export trace to logging

--- java/core/src/main/java/io/temporal/otel/http/HttpWorker.java
+++ java/core/src/main/java/io/temporal/otel/http/HttpWorker.java
@@ -1,11 +1,13 @@
 package io.temporal.otel.http;
 
 import io.temporal.client.WorkflowClient;
+import io.temporal.opentracing.OpenTracingWorkerInterceptor;
 import io.temporal.otel.http.workflow.HttpActivitiesImpl;
 import io.temporal.otel.http.workflow.HttpWorkflowImpl;
 import io.temporal.serviceclient.WorkflowServiceStubs;
 import io.temporal.worker.Worker;
 import io.temporal.worker.WorkerFactory;
+import io.temporal.worker.WorkerFactoryOptions;
 
 public class HttpWorker {
   private static final WorkflowServiceStubs service = WorkflowServiceStubs.newLocalServiceStubs();
@@ -13,7 +15,11 @@ public class HttpWorker {
   public static final String TASK_QUEUE_NAME = "otel-task-queue";
 
   public static void main(String[] args) {
-    WorkerFactory factory = WorkerFactory.newInstance(client);
+    WorkerFactoryOptions factoryOptions =
+        WorkerFactoryOptions.newBuilder()
+            .setWorkerInterceptors(new OpenTracingWorkerInterceptor(TraceUtils.getTraceOptions()))
+            .build();
+    WorkerFactory factory = WorkerFactory.newInstance(client, factoryOptions);
     Worker worker = factory.newWorker(TASK_QUEUE_NAME);
 
     worker.registerWorkflowImplementationTypes(HttpWorkflowImpl.class);
---
+++ java/core/src/main/java/io/temporal/otel/http/TraceUtils.java
@@ -0,0 +1,61 @@
+package io.temporal.otel.http;
+
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.context.propagation.TextMapPropagator;
+import io.opentelemetry.exporter.logging.LoggingSpanExporter;
+import io.opentelemetry.opentracingshim.OpenTracingShim;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import io.opentelemetry.sdk.trace.samplers.Sampler;
+import io.opentelemetry.semconv.ServiceAttributes;
+import io.opentracing.Tracer;
+import io.temporal.opentracing.OpenTracingOptions;
+import io.temporal.opentracing.OpenTracingSpanContextCodec;
+
+public class TraceUtils {
+  private static final String SERVICE_NAME = "temporal-java";
+
+  public static OpenTracingOptions getTraceOptions() {
+    return getTraceOpenTelemetryOptions();
+  }
+
+  private static OpenTracingOptions getTraceOpenTelemetryOptions() {
+    Resource resource =
+        Resource.getDefault()
+            .merge(Resource.builder().put(ServiceAttributes.SERVICE_NAME, SERVICE_NAME).build());
+
+    LoggingSpanExporter spanExporter = LoggingSpanExporter.create();
+
+    SdkTracerProvider tracerProvider =
+        SdkTracerProvider.builder()
+            .addSpanProcessor(
+                BatchSpanProcessor.builder(spanExporter)
+                    .build())
+            .setResource(resource)
+            .setSampler(Sampler.alwaysOn())
+            .build();
+
+    OpenTelemetrySdk openTelemetry =
+        OpenTelemetrySdk.builder()
+            .setPropagators(
+                ContextPropagators.create(
+                    TextMapPropagator.composite(W3CTraceContextPropagator.getInstance())))
+            .setTracerProvider(tracerProvider)
+            .build();
+
+    // create OpenTracing shim and return OpenTracing Tracer from it
+    return getOpenTracingOptionsForTracer(OpenTracingShim.createTracerShim(openTelemetry));
+  }
+
+  private static OpenTracingOptions getOpenTracingOptionsForTracer(Tracer tracer) {
+    OpenTracingOptions options =
+        OpenTracingOptions.newBuilder()
+            .setSpanContextCodec(OpenTracingSpanContextCodec.TEXT_MAP_CODEC)
+            .setTracer(tracer)
+            .build();
+    return options;
+  }
+}
